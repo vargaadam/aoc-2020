9619a8b425dec453ddc0e54b642da6bb111503bd
Varga Adam <vargaad95@gmail.com>
2020-12-02 18:34:50 +0100

    fix: add the correct day 3 key

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -5,5 +5,5 @@ const { p1: D3P1, p2: D3P2 } = require("./day-3");
 module.exports = {
   1: { parts: [D1P1, D1P2], filePath: __dirname + "/day-1/d1_input" },
   2: { parts: [D2P1, D2P2], filePath: __dirname + "/day-2/d2_input" },
-  2: { parts: [D3P1, D3P2], filePath: __dirname + "/day-3/d3_input" },
+  3: { parts: [D3P1, D3P2], filePath: __dirname + "/day-3/d3_input" },
 };
