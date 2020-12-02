cd2658fbbf8f598c46a18745013f03477aa82093
Varga Adam <vargaad95@gmail.com>
2020-12-02 18:34:14 +0100

    feat: only print one day result

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+const { readInput } = require("./utils");
 const calendar = require("./calendar");
 
+const printMessage = (day) => {
+  const { parts, filePath } = calendar[day];
+  const input = readInput(filePath);
+
+  console.log(`Day ${day}:`);
+  console.log(`Task 1: ${parts[0](input)}`);
+  console.log(`Task 2: ${parts[1](input)}`);
+  console.log("-----------------------------");
+};
+
 (() => {
-  const { readInput } = require("./utils");
+  if (process.env.DAY) {
+    printMessage(process.env.DAY);
+    return;
+  }
 
   Object.keys(calendar).forEach((day) => {
-    const { parts, filePath } = calendar[day];
-    const input = readInput(filePath);
-
-    console.log(`Day ${day}:`);
-    console.log(`Task 1: ${parts[0](input)}`);
-    console.log(`Task 2: ${parts[1](input)}`);
-    console.log("-----------------------------");
+    printMessage(day);
   });
 })();
