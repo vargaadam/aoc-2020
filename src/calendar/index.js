4e263db77eba4cc76eee203e7cb23010242550cb
Varga Adam <vargaad95@gmail.com>
2020-12-05 13:22:26 +0100

    feat: automate the display of results

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,13 +1,21 @@
-const { p1: D1P1, p2: D1P2 } = require("./day-1");
-const { p1: D2P1, p2: D2P2 } = require("./day-2");
-const { p1: D3P1, p2: D3P2 } = require("./day-3");
-const { p1: D4P1, p2: D4P2 } = require("./day-4");
-const { p1: D5P1, p2: D5P2 } = require("./day-5");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = {
-  1: { parts: [D1P1, D1P2], filePath: __dirname + "/day-1/d1_input" },
-  2: { parts: [D2P1, D2P2], filePath: __dirname + "/day-2/d2_input" },
-  3: { parts: [D3P1, D3P2], filePath: __dirname + "/day-3/d3_input" },
-  4: { parts: [D4P1, D4P2], filePath: __dirname + "/day-4/d4_input" },
-  5: { parts: [D5P1, D5P2], filePath: __dirname + "/day-5/d5_input" },
-};
+const days = fs
+  .readdirSync(__dirname)
+  .filter((file) => path.extname(file).toLowerCase() !== ".js")
+  .reduce((acc, day) => {
+    const dirPath = path.join(__dirname, day);
+    const filePath = path.join(dirPath, "input");
+
+    const { p1, p2 } = require(dirPath);
+
+    acc[day] = {
+      parts: [p1, p2],
+      filePath,
+    };
+
+    return acc;
+  }, {});
+
+module.exports = days;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const printMessage = (day) => {
   const { parts, filePath } = calendar[day];
   const input = readInput(filePath);
 
-  console.log(`Day ${day}:`);
+  console.log(day);
   console.log(`Task 1: ${parts[0](input)}`);
   console.log(`Task 2: ${parts[1](input)}`);
   console.log("-----------------------------");
@@ -13,7 +13,7 @@ const printMessage = (day) => {
 
 (() => {
   if (process.env.DAY) {
-    printMessage(process.env.DAY);
+    printMessage(`day-${process.env.DAY}`);
     return;
   }
 
